ad427bacd8c9166e2c015a350bafd1b71b56f223
joel carvajal <joel.carvajal@mail.escuelaing.edu.co>
2020-09-17 06:18:23 -0500

    fin

--- servlet/src/main/java/edu/eci/cvds/servlet/jojoServlet.java
+++ servlet/src/main/java/edu/eci/cvds/servlet/jojoServlet.java
@@ -53,4 +53,41 @@ public class jojoServlet extends HttpServlet{
 				responseWriter.write("requerimiento inválido");
 			}
 	}
+	
+	 @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		Writer responseWriter = resp.getWriter();
+		try {
+		   Optional<String> optId = Optional.ofNullable(req.getParameter("id"));
+		   String tempid = optId.isPresent() && !optId.get().isEmpty() ? optId.get() : "";
+		   int id = Integer.parseInt(tempid);
+		   
+    	   Todo todo = Service.getTodo(id);
+    	   if(todo != null) {
+			   resp.setStatus(HttpServletResponse.SC_OK);
+    		   ArrayList<Todo> todoList = new ArrayList<>();
+    		   todoList.add(todo);
+    		   responseWriter.write(Service.todosToHTMLTable(todoList));
+			}else {
+				resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+				responseWriter.write("id Incorrecto");
+			}
+			
+    		}catch (MalformedURLException m){
+				resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+				responseWriter.write("error interno en el servidor");
+			}catch(NumberFormatException n){
+				resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+				responseWriter.write("formato id no valido");
+			}catch (Exception e){
+				resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+				responseWriter.write("requerimiento inválido");
+			}
+	}
 }
+
+
+
+
+
